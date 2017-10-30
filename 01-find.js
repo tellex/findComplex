5ac11d5884fe5b83fad9365a7e81be62b33a9fe9
greenlogic.ar@gmail.com <greenlogic.ar@gmail.com>
2017-10-29 21:10:18 -0300

    First alpha version

---
+++ 01-find.js
@@ -0,0 +1,30 @@
+var findComplex = require('../../custom/find.js');
+var exclude = [];
+var include = [];
+
+module.exports = function(app){
+  models = app.models();
+  models.forEach(function(model){
+    modelName = model.definition.name;
+    dataSource = model.getDataSource();
+    if(dataSource != null){
+       adapter = dataSource.adapter.name;
+    }
+    else{
+	adapter = false;
+    }
+    if(exclude.indexOf(modelName) == -1 && adapter == 'postgresql' && (include.length == 0 || include.indexOf(modelName) != -1)){
+      model.findComplex = function(filter,cb){
+        complex = new findComplex(model);
+        complex.init();
+        complex.find(filter,cb);
+        // cb();
+      }
+      model.remoteMethod('findComplex', {
+               http: {path: '/findComplex', verb: 'POST'},
+               accepts: {arg: 'filter', type: 'object'},
+               returns: {arg: modelName, type: 'array'}});
+
+    }
+  });
+}
